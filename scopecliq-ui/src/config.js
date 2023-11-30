9bf08433d14fa8e1d9b4ee028f6a3820ff741614
maccimac <macci.web@gmail.com>
2023-11-29 20:13:08 -0800

    Deploy

--- scopecliq-ui/src/config.js
+++ scopecliq-ui/src/config.js
@@ -1,4 +1,6 @@
 module.exports = global.config = {
-    API: "http://localhost:8000/api"
-    // API: "https://scopecliq.com/api"
+    // API: "http://localhost:8000/api"
+    API: "https://scopecliq.com/api"
 }
+
+//  "homepage": "https://scopecliq.com/scopecliq/scopecliq-ui/build",
