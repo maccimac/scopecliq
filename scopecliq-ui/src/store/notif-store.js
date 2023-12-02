57be8f449252d644afffb22bbedabb6b093fc89a
maccimac <macci.web@gmail.com>
2023-12-02 05:06:50 -0800

    Add files cleaned

--- scopecliq-ui/src/store/notif-store.js
+++ scopecliq-ui/src/store/notif-store.js
@@ -11,8 +11,6 @@ const notifSlice = createSlice({
     },
   }
 })
-// export const isClient = (state) =>  state.user.isClient;
-// export const isLoggedIn = (state) => state.login.isLoggedIn
 export const notifControl = (state) => state.notif.control
 export const { updateNotif } = notifSlice.actions
 export default notifSlice.reducer
